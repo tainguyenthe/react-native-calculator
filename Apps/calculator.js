df34fe70b971b8e90152c4b60f248973ff6f1069
Tài Nguyễn <tainguyenthe91@gmail.com>
2017-05-02 19:27:49 +0700

    Initial Tip Calculator

---
+++ Apps/calculator.js
@@ -0,0 +1,102 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+import SegmentedControlTab from 'react-native-segmented-control-tab'
+
+// create a component
+class Calculator extends Component {
+	constructor() {
+		super()
+		this.state = {
+			selectedIndex: 0,
+			billAmount: 0,
+			result: 0,
+			tipAmount: 0
+		}
+	}
+
+	segmentValues = () => {
+		return ['10%', '15%', '50%']
+	}
+	
+	handleSegmentChange = (index) => {
+		this.setState({
+			selectedIndex: index
+		})
+		this.handleBillAmountChange(this.state.billAmount, index)
+	}
+ 
+	handleBillAmountChange = (bill, index) => {
+		if(!index && !index !== 0) {
+			index = this.state.selectedIndex
+		}
+
+		bill = parseFloat(bill)
+		var percent = this.segmentValues()[index] // 10%, 15%, 50%
+		percent = parseFloat(percent)/100
+		var result = bill + (bill * percent)
+		var tipAmount = bill * percent
+
+		this.setState({
+			billAmount: bill,
+			tipAmount: tipAmount,
+			result: result
+		})
+	}
+
+	render() {
+		return (
+			<View>
+				<View>
+					<Text>Tips Calculator</Text>
+				</View>
+				
+				<View>
+					<Text>Bill Amount</Text>
+					<TextInput
+						style={{height: 40, borderColor: 'gray', borderWidth: 1}} 
+						placeholder="Type here"
+						onChangeText={(billAmount => this.handleBillAmountChange(billAmount))}
+					/>
+				</View>
+
+				<View>
+					<Text>Tip Amount: 0</Text>
+				</View>
+
+				<View>
+					<Text>Segment Control</Text>
+					<SegmentedControlTab
+						values={this.segmentValues()}
+						selectedIndex={this.state.selectedIndex}
+						onTabPress={index => this.handleSegmentChange(index)}
+					/>
+				</View>
+
+				<View>
+					<Text>Bill input: {this.state.billAmount}</Text>
+					<Text>Tip amount: {this.state.tipAmount}</Text>
+					<Text>Segment control: {this.segmentValues()[this.state.selectedIndex]}</Text>
+				</View>
+
+				<View>
+					<Text>Result: {this.state.result}</Text>
+				</View>
+			</View>
+		);
+	}
+}
+
+// define your styles
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#fff',
+	},
+});
+
+//make this component available to the app
+export default Calculator;
---
+++ index.ios.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Calculator from './Apps/calculator.js'
+
+export default class PreworkCalculator extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+AppRegistry.registerComponent('PreworkCalculator', () => PreworkCalculator);
